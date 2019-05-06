a146589b55adb774e56c4b7441ce3c35c6492b4b
PHENPHICHA <5910110236@psu.ac.th>
2019-05-06 23:53:56 +0700

    Create a Bootstrap 4 Form.

--- src/components/Create.js
+++ src/components/Create.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react';
 export default class Create extends Component {
     render() {
         return (
-            <div>
-                <p>Welcome to Create Component!!</p>
+            <div style={{marginTop: 50}}>
+                <h3>Add New Product</h3>
+                <form>
+                    <div className="form-group">
+                        <label>Add Name:  </label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Add Price: </label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Add" className="btn btn-primary"/>
+                    </div>
+                </form>
             </div>
         )
     }
