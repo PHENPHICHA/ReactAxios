fe102e1db1c47d2be7efe411919d35216c87856c
PHENPHICHA <5910110236@psu.ac.th>
2019-05-06 23:49:49 +0700

    Create a Navigation bar.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Index from './components/Index';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  render() {
+  render() { 
     return (
       <Router>
         <div className="container">
-          <h2>React Axios</h2>
-          <ul>
-            <li><Link to={'/create'}>Create</Link></li>
-            <li><Link to={'/index'}>List</Link></li>
-          </ul>
-          <hr />
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand">React Express App</a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item"><Link to={'/create'} className="nav-link">Create</Link></li>
+                <li className="nav-item"><Link to={'/index'} className="nav-link">List</Link></li>
+              </ul>
+              <hr />
+            </div>
+          </nav> <br />
           <Switch>
               <Route exact path='/create' component={ Create } />
               <Route path='/index' component={ Index } />
